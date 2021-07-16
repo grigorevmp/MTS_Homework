6305219863b8d9e5350e0e5e7d9a9b0e94367a40
Mikhail <16112000m@gmail.com>
2021-07-16 12:01:41 +0300

    - layout-land added

--- app/src/main/java/com/mikhailgrigorev/mts_home/RecyclerViewDecoration.kt
+++ app/src/main/java/com/mikhailgrigorev/mts_home/RecyclerViewDecoration.kt
@@ -8,7 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 class RecyclerViewDecoration
 (
     private val margin_top: Int,
-    private val margin_horizontal: Int
+    private val margin_horizontal: Int,
+    private val spanCount: Int = 2,
+    private val includeEdge: Boolean = false
 ) :
     RecyclerView.ItemDecoration() {
     override fun getItemOffsets(
@@ -18,10 +20,25 @@ class RecyclerViewDecoration
         state: RecyclerView.State
     ) {
         super.getItemOffsets(outRect, view, parent, state)
+
+        val position: Int = parent.getChildAdapterPosition(view)
+        val column = position % spanCount
+        if(includeEdge) {
+            outRect.left = margin_horizontal - column * margin_horizontal / spanCount;
+            outRect.right = (column + 1) * margin_horizontal / spanCount;
+
+            if (position < spanCount) {
+                outRect.top = margin_horizontal;
+            }
+        }
+        else{
+            outRect.left = margin_horizontal
+            outRect.right = margin_horizontal
+        }
+
+
         outRect.top = margin_top
         outRect.bottom = margin_top
-        outRect.left = margin_horizontal
-        outRect.right = margin_horizontal
     }
 
 }
